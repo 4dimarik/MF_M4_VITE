31e2ff2145e0e8a802f5542b5e00cfdfb8d2ddd9
Dritriy Golyshev <4dimariK@gmail.com>
2023-08-13 09:27:23 +0300

    style: mantine ThemeProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import { Container } from '@mantine/core';
-import './App.css';
+import { MantineProvider, ColorSchemeProvider, Container } from '@mantine/core';
 import { AppRouter } from './context/router';
+import { useState } from 'react';
 
 function App() {
+  const [colorScheme, setColorScheme] = useState('light');
+  const toggleColorScheme = (value) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
   return (
     <Container>
-      <AppRouter />
+      <ColorSchemeProvider
+        colorScheme={colorScheme}
+        toggleColorScheme={toggleColorScheme}
+      >
+        <MantineProvider
+          theme={{ colorScheme }}
+          withGlobalStyles
+          withNormalizeCSS
+        >
+          <AppRouter />
+        </MantineProvider>
+      </ColorSchemeProvider>
     </Container>
   );
 }
