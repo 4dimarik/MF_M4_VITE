4435872f829eaa5a34f6c66365b5ef756b51d389
Dritriy Golyshev <4dimariK@gmail.com>
2023-08-13 10:36:34 +0300

    fix: update content MainPage

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,3 +1,14 @@
+import { Flex, Image } from '@mantine/core';
+
 export default function MainPage() {
-  return <div>MainPage</div>;
+  return (
+    <Flex justify="flex-center" align="flex-start" sx={{ width: '100%' }}>
+      <Image
+        mx="auto"
+        fit="contain"
+        src="/Rick-And-Morty-Logo.png"
+        alt="Rick-And-Morty-Logo"
+      />
+    </Flex>
+  );
 }
