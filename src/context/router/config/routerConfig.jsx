2f505202ef81af2862b930d3a8d5e2609aa24e87
Dritriy Golyshev <4dimariK@gmail.com>
2023-08-13 10:25:31 +0300

    feat: add NotFound route

--- src/context/router/config/routerConfig.jsx
+++ src/context/router/config/routerConfig.jsx
@@ -1,12 +1,17 @@
 import { routes } from '../../../configs/routes';
 import IndexLayout from '../../../layouts/IndexLayout';
 import MainPage from '../../../pages/MainPage';
+import { NotFoundPage } from '../../../pages/NotFoundPage';
 
 const routerConfig = [
   {
     path: routes.main.path,
     element: <IndexLayout />,
     children: [{ index: true, element: <MainPage /> }],
+  },
+  {
+    path: '*',
+    element: <NotFoundPage />,
   },
 ];
 
