94d6320a16c505ab5751d4778e6d88877a8135e3
Dritriy Golyshev <4dimariK@gmail.com>
2023-08-14 20:11:00 +0300

    refactor: export default

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -50,7 +50,7 @@ const useStyles = createStyles((theme) => ({
   },
 }));
 
-export function NotFoundPage() {
+export default function NotFoundPage() {
   const { classes } = useStyles();
 
   return (
--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,7 +1,7 @@
 import { Paper, Container, Title } from '@mantine/core';
 import SignIn from '../components/SignIn';
 
-export function SignInPage() {
+export default function SignInPage() {
   return (
     <Container size={420} my={40}>
       <Title align="center" sx={{ fontWeight: 900 }}>
